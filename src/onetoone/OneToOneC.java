85e19971a35839fac9cd1816ae8618d2bee6ad77
Anjihye <anji314@naver.com>
2019-09-15 19:52:16 +0900

    onetoeone_v1.0

---
+++ src/onetoone/OneToOneC.java
@@ -0,0 +1,103 @@
+package onetoone;
+
+import java.awt.*;
+import java.net.*;
+
+import onetoone.OneToOneS.WinListener;
+
+import java.io.*;
+import java.awt.event.*;
+
+
+public class OneToOneC extends Frame implements ActionListener {
+	TextArea display;
+	TextField text;
+	Label lword;
+	Socket client;
+	BufferedWriter output;
+	BufferedReader input;
+	String clientdata="";
+	String serverdata="";
+	
+	public OneToOneC() {
+		super("클라이언트");
+		display=new TextArea("",0,0,TextArea.SCROLLBARS_VERTICAL_ONLY);
+		display.setEditable(false);
+		add(display,BorderLayout.CENTER);
+		
+		Panel pword=new Panel(new BorderLayout());
+		lword =new Label("대화말");
+		text= new TextField(30);
+		text.addActionListener(this);
+		pword.add(lword,BorderLayout.WEST);
+		pword.add(text,BorderLayout.EAST);
+		add(pword,BorderLayout.SOUTH);
+		
+		addWindowListener(new WinListener());
+		setSize(300,200);
+		setVisible(true);
+		
+	}
+	
+	
+	public void runClient() {
+		try {
+			client= new Socket(InetAddress.getLocalHost(),5000);
+			input=new BufferedReader(new InputStreamReader(client.getInputStream()));
+			output= new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+			while(true) {
+				String serverdata=input.readLine();
+				if(serverdata.equals("quit")) {
+					display.append("\n서버와의 접속이 중단되었습니다.");
+					output.flush();
+					break;
+				}else {
+					display.append("\n서버 메세지 : "+serverdata);
+					output.flush();
+				}
+			}
+			client.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		// TODO Auto-generated method stub
+		clientdata= text.getText();
+		try {
+			display.append("\n클라이언트 : "+clientdata);
+			output.write(clientdata+"\r\n");
+			output.flush();
+			text.setText("");
+			if(clientdata.equals("quit")) {
+				client.close();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String args[]) {
+		OneToOneC c= new OneToOneC();
+		c.runClient();
+		}
+
+	class WinListener extends WindowAdapter{
+		public void windowClosing(WindowEvent e) {
+			System.exit(0);
+		}
+	}
+
+	
+	
+	
+	
+
+}
